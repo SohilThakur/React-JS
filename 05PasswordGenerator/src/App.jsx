13b17983332e7e98ff903bf9ff88d9e09b8be8cb
SohilThakur <sohilthakur0011@gmail.com>
2024-04-13 23:27:31 +0530

    update copy

--- 05PasswordGenerator/src/App.jsx
+++ 05PasswordGenerator/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
   const [length, setLength] = useState(8);
   const [numAllowed, setNumAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
+  // useRef hook
+  const passwordRef = useRef(null);
 
   const passwordGenerator = useCallback(() => {
     let pass = "";
@@ -26,6 +28,12 @@ function App() {
     setPassword(pass);
   }, [length, numAllowed, charAllowed, setPassword]);
 
+  const copyPasswordToClipboard = useCallback(() =>{
+    passwordRef.current?.select()
+    passwordRef.current?.setSelectionRange(0,51)
+    window.navigator.clipboard.writeText (password)
+  }, [password])
+
   useEffect(() => {
     passwordGenerator();
   }, [length, numAllowed, charAllowed, passwordGenerator]);
@@ -43,9 +51,11 @@ function App() {
             className="outline-none w-full py-2 px-3"
             placeholder="password"
             readOnly
+            ref={passwordRef}
           />
 
           <button
+            onClick={copyPasswordToClipboard}
             className="outline-none bg-blue-700 text-white px-3 py-0.5
           shrink-0"
           >
